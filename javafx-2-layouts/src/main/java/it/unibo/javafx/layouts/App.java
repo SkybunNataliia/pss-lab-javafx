411b68656efbbf6bae9dd247dabd2927f81212c3
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-12-09 22:37:48 +0100

    Add right pane and style

--- javafx-2-layouts/src/main/java/it/unibo/javafx/layouts/App.java
+++ javafx-2-layouts/src/main/java/it/unibo/javafx/layouts/App.java
@@ -19,21 +19,66 @@ public class App extends Application {
         // Il root è un BorderPane, che divide la finestra in 5 aree: top, bottom, left, right, center.
         // Noi usiamo solo left e center.
         BorderPane root = new BorderPane();
+
+        // Aggiungo i fogli di stile
+        root.getStylesheets().add("dracula.css");
+        root.getStylesheets().add("style.css");
+        root.setId("root");
+
         // Left Pane, contiene il titolo e il form per aggiungere nuovi task.
         VBox leftPane = new VBox();
         leftPane.setId("left-pane");
+        leftPane.setAlignment(Pos.TOP_CENTER);
 
         Label title = new Label("Todo App");
+        title.getStyleClass().addAll("accent", "title-1");
+
         TextField taskField = new TextField();
         Button addButton = new Button("Add");
-
-        leftPane.setAlignment(Pos.TOP_CENTER);
 
         leftPane.getChildren().addAll(title, taskField, addButton);
 
         VBox rightPane = new VBox();
+        rightPane.setId("right-pane");
+        rightPane.setAlignment(Pos.TOP_CENTER);
+        Label tit2 = new Label("Tasks");
+        tit2.getStyleClass().addAll("accent", "title-2");
 
-         // Imposto i pannelli sinistro e centrale come figli del root.
+        // Area dei task, contiene due liste: una per i task da fare e una per i task completati.
+        HBox taskArea = new HBox();
+        VBox.setVgrow(taskArea, Priority.ALWAYS);
+        taskArea.setId("tasks-area");
+
+        VBox todoPane = new VBox();
+        todoPane.setAlignment(Pos.TOP_CENTER);
+        Label toDo = new Label("Todo");
+        toDo.getStyleClass().add("title-3");
+        todoPane.getChildren().add(toDo);
+
+        VBox donePane = new VBox();
+        donePane.setAlignment(Pos.TOP_CENTER);
+        Label done = new Label("Done");
+        done.getStyleClass().add("title-3");
+        donePane.getChildren().add(done);
+
+        // Setta la larghezza minima delle liste
+        HBox.setHgrow(toDo, Priority.ALWAYS);
+        HBox.setHgrow(toDo, Priority.ALWAYS);
+
+        taskArea.getChildren().addAll(toDo, donePane);
+        rightPane.getChildren().addAll(tit2, taskArea);
+
+        // Aggiungo dei task di esempio
+        var task1 = createTaskSection("Task1");
+        var task2 = createTaskSection("Task2");
+        todoPane.getChildren().addAll(task1, task2);
+        
+        // Aggiungo dei task completati di esempio
+        VBox doneTask = new VBox(new Label("Task 2"));
+        doneTask.getStyleClass().add("task");
+        donePane.getChildren().add(doneTask);
+
+        // Imposto i pannelli sinistro e centrale come figli del root.
         root.setLeft(leftPane);
         root.setCenter(rightPane);
 
@@ -52,6 +97,15 @@ public class App extends Application {
 
     // Crea un todo composto da una label e un bottone per segnare il task come completato.
     public static Node createTaskSection(String content) {
-        throw new UnsupportedOperationException("TODO");
+        HBox task = new HBox();
+        task.getStyleClass().add("task");
+
+        Label taskLabel = new Label(content);
+        VBox text = new VBox(taskLabel);
+        Button doneBut = new Button("X");
+        HBox.setHgrow(text,Priority.ALWAYS);
+
+        task.getChildren().addAll(text, doneBut);
+        return task;
     }
 }
